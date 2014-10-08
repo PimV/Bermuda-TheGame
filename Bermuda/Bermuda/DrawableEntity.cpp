e98b8f773d091b991c30ac2f1ff4ef9e83317051
JohanBeekers <johan.beekers@gmail.com>
2014-10-08 23:34:02 +0200

    Small fix

--- Bermuda/Bermuda/DrawableEntity.cpp
+++ Bermuda/Bermuda/DrawableEntity.cpp
@@ -9,9 +9,9 @@ DrawableEntity::DrawableEntity(int id, Image* image)
 void DrawableEntity::draw(Camera camera, SDL_Renderer* renderer)
 {
 	//Only draw if entity is inside the camera view and the buffer area
-	if((getX() - getWidth()) > (camera.getX() - DRAWBUFFER) &&
+	if((getX() + getWidth()) > (camera.getX() - DRAWBUFFER) &&
 		getX() < (camera.getX() + camera.getWidth() + DRAWBUFFER) &&
-		(getY() - getHeight()) > (camera.getY() - DRAWBUFFER) &&
+		(getY() + getHeight()) > (camera.getY() - DRAWBUFFER) &&
 		getY() < (camera.getY() + camera.getHeight() + DRAWBUFFER))
 	{
 		SDL_Rect rect;
