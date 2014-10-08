f9e7d1a7d3326cf2fa94f6673ffb84f47e7e64e8
James Hay <jwhay@live.com>
2014-10-08 12:11:29 +0200

    Removed rabbit class code from main()

--- Bermuda/Bermuda/Main.cpp
+++ Bermuda/Bermuda/Main.cpp
@@ -7,16 +7,8 @@
 #include <future>
 #include <Windows.h>
 
-// Remove
-#include "Rabbit.h"
-#include "Spawnpoint.h"
-
 int main(int argc, char* args[])
 {
-	// Remove:
-	Spawnpoint* sp = new Spawnpoint(2);
-	Rabbit* rabbit = new Rabbit(1, 1, 1, 1, 1, sp);
-
 	//Player *p1 = new Player(2);
 	//Player *p2 = new Player(88);
 	//std::cout << p1->moveSpeed << " " << p2->moveSpeed;
