188b523ec9ca99c91df8b1def4ed45a4374eac36
sagar193 <sagartje1993@hotmail.com>
2014-10-09 13:12:02 +0200

    Hoofdmenu

--- Bermuda/Bermuda/MenuState.cpp
+++ Bermuda/Bermuda/MenuState.cpp
@@ -12,11 +12,25 @@ MenuState::MenuState(void)
 }
 
 void MenuState::init(GameStateManager *gsm) {
-	SDL_Surface* temp = SDL_LoadBMP("menustate.bmp");
+	backgroundTexture = IMG_LoadTexture(gsm->sdlInitializer->getRenderer(), "Textures/green.bmp");
+	if (backgroundTexture == NULL)
+	{
+		std::cout << "Error loading startmenu background" << std::endl << "Error 2" << std::endl;
+		system("pause");
+	}
+	align();
+	playButton = new PlayButton(gsm);
+	exitButton = new ExitButton(gsm);
+	playButton->align(1, totalButtons);
+	exitButton->align(0, totalButtons);
+}
 
-	bg = SDL_ConvertSurfaceFormat(temp, SDL_PIXELFORMAT_UNKNOWN, 0);
-
-	SDL_FreeSurface(temp);
+void MenuState::align()
+{
+	backgroundRect.x = 0;
+	backgroundRect.y = 0;
+	backgroundRect.w = ScreenWidth;
+	backgroundRect.h = ScreenHeight;
 }
 
 void MenuState::cleanup() {
@@ -35,7 +49,9 @@ void MenuState::resume() {
 void MenuState::handleEvents(GameStateManager *gsm) {
 	SDL_Event mainEvent;
 
-	if(SDL_PollEvent(&mainEvent)) {
+	if (SDL_PollEvent(&mainEvent)) {
+		int x, y;
+		SDL_GetMouseState(&x, &y);
 		switch(mainEvent.type) {
 		case SDL_QUIT:
 	
@@ -52,13 +68,14 @@ void MenuState::handleEvents(GameStateManager *gsm) {
 				break;
 			}
 			break;
-		case SDL_MOUSEMOTION: 
-			int x,y;
-			SDL_GetMouseState(&x, &y);
+		case SDL_MOUSEMOTION:
+			playButton->hover(x, y, gsm);
+			exitButton->hover(x, y, gsm);
 			break;
 		case SDL_MOUSEBUTTONDOWN:
 			if (mainEvent.button.button == SDL_BUTTON_LEFT) {
-				gsm->changeGameState(PlayState::Instance());
+				playButton->clicked(x, y, gsm);
+				exitButton->clicked(x, y, gsm);
 			}
 			break;
 		}
@@ -70,31 +87,40 @@ void MenuState::update(GameStateManager *gsm, double dt) {
 }
 
 void MenuState::draw(GameStateManager *gsm) {
-	//Create surface and textures
-	SDL_Surface* img = SDL_LoadBMP("menustate.bmp");
-	SDL_Texture* texture = SDL_CreateTextureFromSurface(gsm->sdlInitializer->getRenderer(), img);
-
-
-
-	Button* startButton = new Button(150,75,150,75,"start.bmp");
-	Button* exitButton = new Button(150,165,150,75,"exit.bmp");
-
-	//Add texture to renderer
 	gsm->sdlInitializer->clearScreen();
-
-	gsm->sdlInitializer->drawTexture(texture, NULL);
-	startButton->render(gsm->sdlInitializer->getRenderer());
-	exitButton->render(gsm->sdlInitializer->getRenderer());
-
+	SDL_RenderCopy(gsm->sdlInitializer->getRenderer(), backgroundTexture, NULL, &backgroundRect);
+	playButton->draw(gsm);
+	exitButton->draw(gsm);
 	gsm->sdlInitializer->drawScreen();
 
-	//Clean created textures/surfaces
-	SDL_DestroyTexture(texture);  
-	SDL_FreeSurface(img); 
+//	//Create surface and textures
+//	SDL_Surface* img = SDL_LoadBMP("menustate.bmp");
+//	SDL_Texture* texture = SDL_CreateTextureFromSurface(gsm->sdlInitializer->getRenderer(), img);
+//
+//
+//
+//	Button* startButton = new Button(150,75,150,75,"start.bmp");
+//	Button* exitButton = new Button(150,165,150,75,"exit.bmp");
+//
+//	//Add texture to renderer
+//	gsm->sdlInitializer->clearScreen();
+//
+//	gsm->sdlInitializer->drawTexture(texture, NULL);
+//	startButton->render(gsm->sdlInitializer->getRenderer());
+//	exitButton->render(gsm->sdlInitializer->getRenderer());
+//
+//	gsm->sdlInitializer->drawScreen();
+//
+//	//Clean created textures/surfaces
+//	SDL_DestroyTexture(texture);  
+//	SDL_FreeSurface(img); 
 }
 
 
 
 MenuState::~MenuState(void)
 {
+	SDL_DestroyTexture(backgroundTexture);
+	delete playButton;
+	delete exitButton;
 }
--- Bermuda/Bermuda/MenuState.h
+++ Bermuda/Bermuda/MenuState.h
@@ -1,16 +1,30 @@
 #pragma once
+#include "ExitButton.h"
+#include "PlayButton.h"
+#include <SDL_image.h>
+#include <SDL.h>
 #include "igamestate.h"
 #include "header_loader.h"
 class MenuState :
 	public IGameState
 {
 private:
+	SDL_Texture* backgroundTexture;
+	SDL_Rect backgroundRect;
+	static const int totalButtons = 2;
+
 	static MenuState m_MenuState;
 
 	SDL_Surface* bg;
 	int alpha;
 
 public:
+	//TODO: put buttons in array
+	PlayButton* playButton;
+	ExitButton* exitButton;
+
+	void align();
+
 	MenuState(void);
 	void init(GameStateManager *gsm) ;
 	void cleanup();
