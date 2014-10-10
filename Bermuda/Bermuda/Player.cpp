d8cab4bcb63337a5444e3c700c3394054e9d89a5
basvandenheuvel <bassie-boy@live.nl>
2014-10-10 14:00:25 +0200

    collision box berekening goed gemaakt
    niet overschrijven bitches

--- Bermuda/Bermuda/Player.cpp
+++ Bermuda/Bermuda/Player.cpp
@@ -17,9 +17,13 @@ Player::Player(int id, double moveSpeed, Camera* camera)
 	this->dy = 0;
 	this->maxSpeed = 6;
 
+	//Berekening van collisionx verbeterd! DIT IS JUISTE VERSIE
+	//	this->setCollisionWidth(this->getWidth()/4);
+	//  this->setCollisionX((this->getWidth() - this->getCollisionWidth()) / 2);
+	//MERGE DEZE JUISTE VERSIE NIET WEG
 	this->setCollisionHeight(this->getHeight());
-	this->setCollisionWidth(this->getWidth()/2);
-	this->setCollisionX(this->getWidth()/4);
+	this->setCollisionWidth(this->getWidth()/4);
+	this->setCollisionX((this->getWidth() - this->getCollisionWidth()) / 2);
 	this->setCollisionY(0);
 
 	this->mapHeight = this->getHeight();
