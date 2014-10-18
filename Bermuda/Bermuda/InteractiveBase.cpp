3971d44e7c557c60ca401209af926d5ffdcfeb9f
rhsattev <rhsattev@avans.nl>
2014-10-18 14:45:48 +0200

    feedback verwerkt

---
+++ Bermuda/Bermuda/InteractiveBase.cpp
@@ -0,0 +1,11 @@
+#include "InteractiveBase.h"
+
+
+InteractiveBase::InteractiveBase(void)
+{
+}
+
+
+InteractiveBase::~InteractiveBase(void)
+{
+}
--- Bermuda/Bermuda/InteractiveBase.h
+++ Bermuda/Bermuda/InteractiveBase.h
@@ -7,15 +7,12 @@ public:
 	InteractiveBase(void);
 	virtual ~InteractiveBase(void);
 
+	virtual void interact();
 	virtual void setRangeValue(int value) = 0;
 	virtual int getRangeValue() = 0;
-	virtual void setLocationRect(SDL_Rect rect) = 0;
-	virtual SDL_Rect getLocationRect() = 0;
 
 private:
 	int rangeValue;
-	SDL_Rect interactiveLocationRect;
-	SDL_Rect interactiveRangeRect;
 
 	virtual void createRangeRect() = 0;
 };
