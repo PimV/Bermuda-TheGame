817cd022614b842e113e79e4cb9149b570f094bf
basvandenheuvel <bassie-boy@live.nl>
2014-11-06 19:13:44 +0100

    fish object afgemaakt (zonder item aanmaken in inv)

--- Bermuda/Bermuda/Fish.cpp
+++ Bermuda/Bermuda/Fish.cpp
@@ -18,19 +18,60 @@ Fish::Fish(int id, double x, double y, int chunkSize, Image* fishImage) :
 	this->currentInteractTime = 0;
 }
 
-void Fish::interact(Player* player) {
-	//if (player->getInventory()->hasAxe()) {
-		//InteractableEntity::interact(player);
+void Fish::interact(Player* player) 
+{
+	//TODO: check if player has fishingrod or fishing harpoon
+	InteractableEntity::interact(player);
 
-		//if (this->trackInteractTimes()) {
-		//	this->setDestroyedState();
-		//	player->getInventory()->addItem(new ItemWood());
-		//	player->getStatusTracker()->treeCut();
-		//}
-	//}
+	if (this->trackInteractTimes()) {
+		this->setDestroyedState();
+		std::cout << "FISH ADDED!" << std::endl;
+		//TODO: add item fish in inventory
+		//TODO: add fish caught in statustracker
+	}
+}
+
+void Fish::update(double dt) 
+{
+	if (this->destroyed) {
+		this->timeSinceDestroy += GameStateManager::Instance()->getUpdateLength() * dt;
+		if (this->timeSinceDestroy > respawnTime) {
+			this->respawn();
+		}
+	}
+}
+
+void Fish::respawn() 
+{
+	this->destroyed = false;
+	this->timeSinceDestroy = 0;
+	this->setDrawImage(this->fishImage);
+	this->getMainEntityContainer()->getRespawnContainer()->remove(this);
+	this->getMainEntityContainer()->getInteractableContainer()->add(this);
+	this->getMainEntityContainer()->getDrawableContainer()->add(this);
+}
+
+void Fish::setDestroyedState() 
+{
+	this->destroyed = true;
+	//TODO: fish does only have 1 image.... nullptr works???
+	this->setDrawImage(nullptr);
+	this->getMainEntityContainer()->getRespawnContainer()->add(this);
+	this->getMainEntityContainer()->getInteractableContainer()->remove(this);
+	this->getMainEntityContainer()->getDrawableContainer()->remove(this);
+	currentInteractTime = 0;
 }
 
 
 Fish::~Fish()
 {
+	this->getMainEntityContainer()->getDrawableContainer()->remove(this);
+	if(this->destroyed) 
+	{ 
+		this->getMainEntityContainer()->getRespawnContainer()->remove(this); 
+	}
+	else 
+	{ 
+		this->getMainEntityContainer()->getInteractableContainer()->remove(this); 
+	}
 }
