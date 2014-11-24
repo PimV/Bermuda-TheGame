49aaaefd1c56f4eba897d255f4917a2c6be1901d
Pim Verlangen <pim.verlangen@gmail.com>
2014-11-24 10:01:40 +0100

    new rock

--- Bermuda/Bermuda/ItemFactory.cpp
+++ Bermuda/Bermuda/ItemFactory.cpp
@@ -11,10 +11,10 @@ void ItemFactory::loadItemTileSets(ImageLoader* imgLoader)
 	itemImages["carrot"] = imgLoader->getMapImage(imgLoader->loadTileset("Items\\ItemCarrot.png", 30, 22));
 	itemImages["fish"] = imgLoader->getMapImage(imgLoader->loadTileset("Items\\ItemFish.png", 30, 24));
 	itemImages["apple"] = imgLoader->getMapImage(imgLoader->loadTileset("Items\\ItemApple.png", 30,34));;
-	itemImages["rock"] = imgLoader->getMapImage(imgLoader->loadTileset("Items\\ItemRock.png", 30,22));
+	itemImages["rock"] = imgLoader->getMapImage(imgLoader->loadTileset("Items\\ItemRock.png", 27,28));
 	itemImages["wood"] = imgLoader->getMapImage(imgLoader->loadTileset("Items\\ItemLogs.png", 32,25));
 	itemImages["gold"] = imgLoader->getMapImage(imgLoader->loadTileset("Items\\ItemGold.png", 34,22));
-	itemImages["water"] = imgLoader->getMapImage(imgLoader->loadTileset("Items\\ItemWater.png", 22,34));
+	itemImages["water"] = imgLoader->getMapImage(imgLoader->loadTileset("Items\\ItemWater.png", 20,30));
 	itemImages["axe"] = imgLoader->getMapImage(imgLoader->loadTileset("Items\\ToolAxe.png", 22,27));
 	itemImages["pickaxe"] = imgLoader->getMapImage(imgLoader->loadTileset("Items\\ToolPickaxe.png", 32,32));
 	itemImages["flint"] = imgLoader->getMapImage(imgLoader->loadTileset("Items\\ToolFlint.png", 38,22));;
