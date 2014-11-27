8a88e562839f0425b657dcd74add937366f5d0a0
Johan Beekers <johan.beekers@gmail.com>
2014-11-28 00:09:33 +0100

    cout removed

--- Bermuda/Bermuda/Campfire.cpp
+++ Bermuda/Bermuda/Campfire.cpp
@@ -10,7 +10,6 @@ Campfire::Campfire(int id, double x, double y, int chunkSize, int firstImgID) :
 	PlayState::Instance()->getMainEntityContainer()->getDrawableContainer()->add(this);
 	PlayState::Instance()->getMainEntityContainer()->getAnimatingContainer()->add(this);
 	PlayState::Instance()->getMainEntityContainer()->getCollidableContainer()->add(this);
-	std::cout << "full: " << GameTimer::Instance()->getFullDayLength() << std::endl;
 	this->creationTime = GameTimer::Instance()->getGameTime();
 	this->lifeTime = GameTimer::Instance()->getNightLength() + 40000;
 	setAnimation(1, 3, 100, GameTimer::Instance()->getNightLength() + 20000, 0);
