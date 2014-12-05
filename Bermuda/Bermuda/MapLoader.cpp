d7e22675330ffe1165ffa3a1c51a93795f5564c1
basvandenheuvel <bassie-boy@live.nl>
2014-12-05 12:30:48 +0100

    Alle Capacity() omgezet naar Size() in maploader

--- Bermuda/Bermuda/MapLoader.cpp
+++ Bermuda/Bermuda/MapLoader.cpp
@@ -126,7 +126,7 @@ void MapLoader::extractMapInfo(Document& d)
 	Value& tilesets = d["tilesets"];
 	createTileSets(tilesets);
 
-	for(size_t i = 0; i < d["layers"].Capacity(); i++)
+	for(size_t i = 0; i < d["layers"].Size(); i++)
 	{
 		Value& layer = d["layers"][i];
 		string layerName = layer["name"].GetString();
@@ -152,12 +152,12 @@ void MapLoader::createTileSets(Value& tilesets)
 {
 	double startLoadPercentage = loadPercentage;
 	double loadWeight = 20;
-	double totalTilesets = tilesets.Capacity();
+	double totalTilesets = tilesets.Size();
 	double processedTilesets = 0;
 	int tempPercentage = loadPercentage;
 	loadStatus = "Loading tilesets.";
 
-	for(size_t i = 0; i < tilesets.Capacity(); i++)
+	for(size_t i = 0; i < tilesets.Size(); i++)
 	{
 		Value& tileset = tilesets[i];
 		string imgName = tileset["image"].GetString();
@@ -240,12 +240,12 @@ void MapLoader::createObjects(Value& objects)
 
 	double startLoadPercentage = loadPercentage;
 	double loadWeight = 20;
-	double totalObjects = objects.Capacity();
+	double totalObjects = objects.Size();
 	double processedObjects = 0;
 	int tempPercentage = loadPercentage;
 	loadStatus = "Creating objects.";
 
-	for(size_t j = 0; j < objects.Capacity(); j++)
+	for(size_t j = 0; j < objects.Size(); j++)
 	{
 		Value& object = objects[j];
 		int objectID = object["gid"].GetInt();
@@ -356,12 +356,12 @@ void MapLoader::createSpawnPoints(Value& spawnpoints)
 {
 	double startLoadPercentage = loadPercentage;
 	double loadWeight = 10;
-	double totalSpawnpoints = spawnpoints.Capacity();
+	double totalSpawnpoints = spawnpoints.Size();
 	double processedSpawnpoints = 0;
 	int tempPercentage = loadPercentage;
 	loadStatus = "Creating spawnpoints.";
 
-	for(size_t j = 0; j < spawnpoints.Capacity(); j++)
+	for(size_t j = 0; j < spawnpoints.Size(); j++)
 	{
 		Value& object = spawnpoints[j];
 		Value& properties = object["properties"];
